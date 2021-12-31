a8d6d188ef87f9a0d8079b0320d7e72b61109787
eatplaynap <eatplaynap329@gmail.com>
2021-12-31 17:37:27 +0900

    LocalStorageにデータ保存・読み出しできるようにした

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
+const taskStorage = {
+  fetch() {
+    const tasks = JSON.parse(localStorage.getItem('task') || '[]' )
+    tasks.forEach((task, index) => {
+      task.id = index
+    })
+    taskStorage.uid = tasks.length
+    return tasks
+  },
+  save(tasks) {
+    localStorage.setItem('task', JSON.stringify(tasks))
+  }
+}
+
 const app = Vue.createApp({
   data() {
     return {
-      tasks: [
-        { id: 1, text: '餃子作る' }
-      ]
+      newTask: '',
+      tasks: taskStorage.fetch()
+    }
+  },
+  watch: {
+    tasks: {
+      handler(tasks) {
+        taskStorage.save(tasks)
+      },
+      deep: true
+    }
+  },
+  methods: {
+    addTask() {
+      this.tasks.push(this.newTask)
+      this.newTask = ''
+    },
+    deleteTask(index){
+      this.tasks.splice(index, 1)
     }
   }
 })
